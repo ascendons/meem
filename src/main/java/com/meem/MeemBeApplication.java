0b19576aad468a48bf89d71de5251f707e1541ba
pankajThakur1874 <pankajthakur1874@gmail.com>
2025-06-01 00:39:29 +0530

    initial commit

---
+++ src/main/java/com/meem/MeemBeApplication.java
@@ -0,0 +1,13 @@
+package com.meem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeemBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeemBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/meem/MeemBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeemBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
